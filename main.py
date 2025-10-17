1ef926813d2c79ede8b5a3f401a5e50d603061d4
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-10-17 10:05:21 +0800

    Added sales by platform (with visualization)

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def fill_null_val(df, column, value):
         df.fillna({column: value}, inplace=True)
     )
 
+
 # check how many duplicates
 def check_duplicated_val(df, **kwargs):
     return df.duplicated(**kwargs).sum()
+
 
 # drop duplicated value with columns
 def drop_duplicated_val(df, **kwargs):
@@ -26,9 +28,18 @@ def top_10(df, sales, name, limited):
         .head(limited)
     )
 
+
 def split_val(value, split, axis, side):
     return (
         np.array_split(value, split, axis=axis)[side]
+    )
+
+
+def sales_by_platform(df, sales, platform):
+    return (
+        df.groupby(platform)[sales]
+        .sum()
+        .reset_index()
     )
 
 try:
@@ -52,25 +63,35 @@ try:
     reshape_top10_global_sales = top10_global_sales_2d.reshape(-1)
     reshape_top10_global_games = top10_global_games_2d.reshape(-1)
 
+    # Global Sales by Platform
+    platform_sale = sales_by_platform(df, 'Global_Sales', 'Platform')
+
+    print(platform_sale)
 
     # PRINT TERMINAL
-    print(top10.values)
+    print("\nTop 10 best-selling games globally (via Global_Sales)")
+    print(top10)
 
 
     # VISUALIZATION
     x = np.arange(10)
 
-    print(x)
+    # plt.title("Top 10 Best-selling Games Globally", fontweight='bold', fontsize=20)
+    # plt.bar(reshape_top10_global_games, reshape_top10_global_sales, edgecolor='black', color="#509e67")
+    # plt.grid(True, alpha=0.3)
+    # plt.xlabel('Games', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
+    # plt.ylabel('Revenues (by Million $)', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
+    # plt.xticks(rotation=35)
+    # plt.tight_layout()
+    # plt.show()
 
-    plt.title("Top 10 Best-selling Games Globally", fontweight='bold')
-    plt.bar(reshape_top10_global_games, reshape_top10_global_sales, edgecolor='black', color="#509e67")
-    plt.grid(True, alpha=0.3)
-    plt.xlabel('Games', fontfamily='Comic Sans MS', fontweight='bold')
-    plt.ylabel('Revenues (by Million $)', fontfamily='Comic Sans MS', fontweight='bold')
-    plt.xticks(rotation=35)
-    plt.tight_layout()
+    sales_by_platform_ticks = np.arange(31)
+    plt.title("Sales by Platform", fontweight='bold', fontsize=20)
+    plt.barh(platform_sale['Platform'], platform_sale['Global_Sales'], edgecolor='black', color="#C886F7F6")
+    plt.xlabel('Sales in Millions (Global Sales)', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
+    plt.grid(True, alpha=0.4, linestyle='--')
+    plt.ylabel('Platform', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
     plt.show()
-
 
 except Exception as e:
 
