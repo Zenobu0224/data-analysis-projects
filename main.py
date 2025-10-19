f091db624c05928e4d7d7961d47d4c84f0035e83
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-10-19 10:07:22 +0800

    Done task 2

--- main.py
+++ main.py
@@ -28,18 +28,25 @@ def top_10(df, sales, name, limited):
         .head(limited)
     )
 
-
-def split_val(value, split, axis, side):
-    return (
-        np.array_split(value, split, axis=axis)[side]
+def top_publishers(df, publishers, sales, limited, ascen):
+    return  (
+        df.groupby(publishers)[sales]
+        .sum()
+        .sort_values(ascending=ascen)
+        .head(limited)
     )
 
-
+# Sales by platform
 def sales_by_platform(df, sales, platform):
     return (
         df.groupby(platform)[sales]
         .sum()
         .reset_index()
+    )
+
+def sum_of_all_sales(df, n_america_sales, europe_sales, japan_sales, other_sales):
+    return  (
+        df[[n_america_sales, europe_sales, japan_sales, other_sales]].sum()
     )
 
 try:
@@ -55,35 +62,41 @@ try:
     drop_duplicated_val(df, subset=['Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales'])
 
     # top 10 best selling games(global sales)
-    top10 = top_10(df, 'Global_Sales', 'Name', 10)
-
-    top10_global_sales_2d = split_val(top10.values, 2, 1, 0)
-    top10_global_games_2d =    split_val(top10.values, 2, 1, 1)
-
-    reshape_top10_global_sales = top10_global_sales_2d.reshape(-1)
-    reshape_top10_global_games = top10_global_games_2d.reshape(-1)
+    top10_games = top_10(df, 'Global_Sales', 'Name', 10)
 
     # Global Sales by Platform
     platform_sale = sales_by_platform(df, 'Global_Sales', 'Platform')
 
-    print(platform_sale)
+    # Sales over the years
+    sales_per_year = sales_by_platform(df, 'Global_Sales', 'Year')
 
-    # PRINT TERMINAL
-    print("\nTop 10 best-selling games globally (via Global_Sales)")
-    print(top10)
+    overall_regional_sales = sum_of_all_sales(df, 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales')
 
+    sales_by_genre = sales_by_platform(df, 'Global_Sales', 'Genre')
+
+    top10_publishers = top_publishers(df, 'Publisher', 'Global_Sales', 10, False)
+
+    print(top10_publishers)
+
+    # print(sales_per_year)
+
+    # print(platform_sale)
+
+    # # PRINT TERMINAL
+    # print("\nTop 10 best-selling games globally (via Global_Sales)")
+    # print(top10_games)
 
     # VISUALIZATION
     x = np.arange(10)
 
-    # plt.title("Top 10 Best-selling Games Globally", fontweight='bold', fontsize=20)
-    # plt.bar(reshape_top10_global_games, reshape_top10_global_sales, edgecolor='black', color="#509e67")
-    # plt.grid(True, alpha=0.3)
-    # plt.xlabel('Games', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
-    # plt.ylabel('Revenues (by Million $)', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
-    # plt.xticks(rotation=35)
-    # plt.tight_layout()
-    # plt.show()
+    plt.title("Top 10 Best-selling Games Globally", fontweight='bold', fontsize=20)
+    plt.bar(top10_games['Name'], top10_games['Global_Sales'], edgecolor='black', color="#509e67")
+    plt.grid(True, alpha=0.3)
+    plt.xlabel('Games', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
+    plt.ylabel('Revenues (by Million $)', fontfamily='Comic Sans MS', fontweight='bold', fontsize=17)
+    plt.xticks(rotation=35)
+    plt.tight_layout()
+    plt.show()
 
     sales_by_platform_ticks = np.arange(31)
     plt.title("Sales by Platform", fontweight='bold', fontsize=20)
@@ -91,6 +104,45 @@ try:
     plt.xlabel('Sales in Millions (Global Sales)', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
     plt.grid(True, alpha=0.4, linestyle='--')
     plt.ylabel('Platform', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
+    plt.show()
+
+    sale_per_year_index = np.arange(40)
+    plt.title('Sales Over the Years', fontweight='bold', fontsize=20)
+    plt.plot(sale_per_year_index, sales_per_year['Global_Sales'], marker='o', color='#000000')
+    plt.xticks(ticks=sale_per_year_index, labels=sales_per_year['Year'], rotation=45)
+    plt.grid(True, alpha=0.3)
+    plt.xlabel('Year', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
+    plt.ylabel('Global Sales (by Million $)', fontweight='bold', fontsize=13, fontfamily='Comic Sans MS')
+    plt.tight_layout()
+    plt.show()
+
+    labels = ['North America Sales', 'Europe  Sales', 'Japan Sales', 'Other Sales']
+    pos = np.arange(len(labels))
+    plt.title("Regional Sale Comparison", fontweight='bold', fontsize=20)
+    plt.bar(pos, overall_regional_sales.values, label=overall_regional_sales.index, color=["#A96FE7", "#84E7C1", "#4085EE", "#D261C5"], edgecolor='black')
+    plt.xticks(ticks=pos, labels=labels)
+    plt.grid(True, alpha=0.2)
+    plt.xlabel('Region', fontweight='bold', fontsize=13, fontfamily="Comic Sans MS")
+    plt.ylabel('Sales (by Million $)', fontweight='bold', fontsize=13, fontfamily="Comic Sans MS")
+    plt.legend()
+    plt.show()
+
+    colors = [
+    '#FF6B6B', '#FFA45B', '#FFD93D', '#6BCB77', '#4D96FF', '#89CFF0',
+    '#B983FF', '#F25287', '#008891', '#FFD3B6', '#E5989B', '#A8A4CE'
+    ]   
+    plt.title("Sales by Genre", fontweight='bold', fontsize=20)
+    plt.pie(sales_by_genre['Global_Sales'], labels=sales_by_genre['Genre'], startangle=90, colors=colors, autopct='%1.1f%%')
+    plt.legend(loc='upper left', bbox_to_anchor=(-0.4, 1.1))
+    plt.show()
+
+    plt.title("Top Publishers", fontweight='bold', fontsize=20)
+    plt.bar(top10_publishers.index, top10_publishers.values, edgecolor='black', color="#485ED6")
+    plt.xlabel("Publisher", fontsize=13, fontweight='bold', fontfamily='Comic Sans MS')
+    plt.ylabel("Global Sales (by Million $)", fontsize=13, fontweight='bold', fontfamily='Comic Sans MS')
+    plt.grid(True, alpha=0.3)
+    plt.xticks(rotation=35)
+    plt.tight_layout()
     plt.show()
 
 except Exception as e:
